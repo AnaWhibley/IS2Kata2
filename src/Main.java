522a04e3dd88492943d186422d7b9df3fc5729f7
Ana <ana@MacBook-Pro-de-Ana.local>
2019-10-02 09:43:42 +0100

    V2 finished - HistogramDisplay class setted and used in main class

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Histogram histogram = new Histogram(2,2,4,5,6,4,5,6,8,8,10,12);
-        for (int value : histogram.getValues()) {
-            System.out.println(value + ": " + histogram.getCount(value));
-        }
+        HistogramDisplay display = new HistogramDisplay(new Histogram(2,2,4,5,6,4,5,6,8,8,10,12));
+        display.execute();
     }
 }
